adce979eb67b9ea5f299b3e7a863adb4546c7da1
pr-124 <112579333+pr-124@users.noreply.github.com>
2022-09-23 22:47:24 +0000

    Final commit

--- hello_dask_books.py
+++ hello_dask_books.py
@@ -6,48 +6,20 @@ ddf = dask_query_books()
 #print("One record from the enron dataset with compute:")
 #print(ddf["title"][0].compute())
 
-#print(ddf["title"][0:10].compute())  
+)  
 
-#print(len(ddf['region'].unique()))
-#print(ddf[(ddf.title == 'Safari') & (ddf.region == 'Chile')].compute())
 
-#x =ddf[ddf["title"] == "Safari"]["artist"].compute()
-#print(x.head(n=1))
 
-#def get_book(title:str):
-    #x =ddf[ddf["title"] == title]["artist"].compute()
-   # return x.head(n=1)
 
 def get_book(title:str):
     x =ddf[ddf["title"] == title]["authors"].compute()
     return x.head(n=1)
-#print(get_artist("Safari"))
-
-#def get_artist(name):
-   # x = ddf[["title" == name]].compute()
-   # artist_name = x["artist"]
-  #  return x.head(n=1)
-
-#print(get_artist("Safari"))
-
-#x =ddf[ddf["title"] == title]["artist"].compute()
-#return x.head(n=1)
 
 
-#print(ddf[ddf.chart == top200])
-# streams = ddf.groupby('region')['streams'].sum().reset_index()
-# streams['percent_streams'] = streams['streams']/streams['streams'].sum()
-#streams['region'] = streams.apply(lambda x: x['region'] if x['percent_streams'] >= .01 else 'Other', axis=1, meta=(None, 'object'))
-#streams = streams.groupby('region')['percent_streams'].sum().reset_index().round(3).sort_values(by='percent_streams')
-#print(streams.compute())
+
 
 def get_author_rating(author_n:str):
     author_r = ddf.groupby('authors')['average_rating'].mean().reset_index()
     author_rating= author_r[author_r['authors'] == author_n].compute()
     return author_rating.head(n=1)
-
-
-#def get_country(country:str):
- #   country_name = streams[streams['region'] == country].compute()
-  #  return country_name.head(n=1)
 
--- main_app.py
+++ main_app.py
@@ -10,26 +10,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Let's find the author of the book by using  the url/authors/{book_name} and the average rating of all authors by using the url/rating/{author_name}"}
-
-
-#@app.get("/add/{num1}/{num2}")
-#async def add(num1: int, num2: int):
-    #"""Add two numbers together"""
-
-   # total = num1 + num2
-    #return {"total": total}
-
-
-#@app.get("/charts/{title}")
-#async def read_item(title):
- #   return {"title": title}
-
-
-# @app.get("/artist/{a}")
-# async def q_artist(a:str):
-  #  answer = get_artist(a) 
-   # return {"artist": answer}
-
+    
 @app.get("/authors/{book_name}")
 async def q_authors(book_name:str):
     answer = get_book(book_name) 
@@ -40,10 +21,7 @@ async def query(author_name:str):
     result = get_author_rating(author_name)
     return {"answer": result}
 
-#@app.get("/region/{c_name}")
-#async def query(c_name:str):
-  #  result = get_country(c_name)
-  #  return {"answer": result}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
